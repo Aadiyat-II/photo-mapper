4aa1cddeede5fc295cf57b741e1b2e2de2a92a23
Aadiyat Ahmad <aadiyat.ahmad@outlook.com>
2025-09-04 17:21:06 -0400

    Configure daphne asgi app

--- photo_mapper_webserver/photo_mapper_webserver/settings.py
+++ photo_mapper_webserver/photo_mapper_webserver/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,6 +68,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'photo_mapper_webserver.wsgi.application'
+ASGI_APPLICATION = 'photo_mapper_webserver.asgi.application'
 
 
 # Database
