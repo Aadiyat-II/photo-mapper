74def3ce4dd03928dec0fc91674ca4eab41a108c
Aadiyat Ahmad <aadiyat.ahmad@outlook.com>
2025-09-04 19:32:32 -0400

    models for photos and tags

--- photo_mapper_webserver/photo_gis/models.py
+++ photo_mapper_webserver/photo_gis/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+import os
+import uuid
+from pathlib import Path
+from django.contrib.gis.db import models
+from django.conf import settings
 
 # Create your models here.
+
+def  photo_directory_path(instance, filename):
+    ext = filename.split('.')[-1]
+    unique_filename = f"{instance.id.hex}.{ext}"
+    return os.path.join("uploads", str(instance.user.id), unique_filename).replace('\\', '/')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class Photo(models.Model):    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=photo_directory_path)
+    location = models.PointField()
+    datetime = models.DateTimeField()
+    tags = models.ManyToManyField(Tag, related_name='tags')
